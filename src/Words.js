1fdb31f357f60f89d1fa62242c783216a5061413
nusoloman <nusretatif08@gmail.com>
2022-10-15 19:53:05 +0300

    uygulama calisir duruma getirildi

---
+++ src/Words.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import randomWords from 'random-words'
+
+
+import {Button, Card, Input, Table, } from "reactstrap";
+
+
+export default class Words extends Component {
+  state = {
+    words: [],
+    index : 0,
+    inputValue : "",
+    backgroundColor:"",
+  };
+
+  onFocus = () => {
+    document.getElementById("input").placeholder = "";
+  }
+  onBlur = () => {
+    document.getElementById("input").placeholder = "Başalamak için butona tıklayınız";
+  }
+  getWords = () => {
+    const allWords = document.querySelectorAll('.word');
+    allWords.forEach(word => {
+      word.style.backgroundColor = 'white';
+    });
+    
+    const words = randomWords(10);
+    this.setState({ words });
+    this.setState({index:0});
+
+   
+  };
+
+  checkWord = (e) => {
+    document.getElementById(this.state.index).style.backgroundColor = "yellow";
+    const { words } = this.state;
+    let { value } = e.target;
+    this.setState({inputValue : value})
+    value = value.toLowerCase().split(" ");
+    console.log(value)
+
+    if (value.length === 2) {
+    if (words[this.state.index] === value[0]) {
+    console.log("true")
+    this.setState({index: this.state.index+1});
+    document.getElementById(this.state.index).style.backgroundColor = "green";
+    this.setState({inputValue: ""});
+    
+  }
+  else{
+    console.log("false")
+    this.setState({index: this.state.index+1});
+    document.getElementById(this.state.index).style.backgroundColor = "red";
+    this.setState({inputValue: ""});
+  }
+  }
+  }
+  
+  render() {
+    
+    return (
+
+      <div style={{textAlign:"center"}}>
+        <Card>
+          {this.state.words.map((word, index) => (
+            <div>
+              <h3 id={index} className='word' style={{display: "inline-block" , backgroundColor: this.state.backgroundColor}}>{word}</h3>
+            </div>
+          ))}
+
+        </Card>
+
+        <Input id="input" type='text' onFocus={this.onFocus} onBlur={this.onBlur} value={this.state.inputValue} onChange={(e)=> this.checkWord(e)} ></Input>
+
+        <Button onClick={this.getWords}> Get</Button>
+
+        
+      </div>
+
+ ) } 
+}
